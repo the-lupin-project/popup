c83d2481f4a6a442385b81c19e4bc02b02f292e1
Neftali Dominguez <n.l.dominguez23@gmail.com>
2019-08-03 12:08:32 -0700

    fixed the create table queries to use promises so they can run sequentially, since they depend on each other

--- server/data/database.js
+++ server/data/database.js
@@ -11,36 +11,41 @@ const uri = 'postgres://whznpqqr:jnYpb12XBSAxEFih-y0TiPFpBCV-NIUu@raja.db.elepha
  */
 const pool = new Pool({ connectionString: uri });
 
-pool.query(`CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  first_name VARCHAR (255) NOT NULL,
-  last_name VARCHAR (255) NOT NULL,
-  email VARCHAR (255) NOT NULL,
-  password VARCHAR (255) NOT NULL,
-  role VARCHAR (255) NOT NULL
-)`);
-
-pool.query(`CREATE TABLE IF NOT EXISTS events (
-  id SERIAL PRIMARY KEY NOT NULL,
-  title VARCHAR (255) NOT NULL,
-  description VARCHAR (500) NOT NULL,
-  max_attendees INTEGER NOT NULL,
-  host INTEGER REFERENCES users(id) NOT NULL,
-  location VARCHAR (255) NOT NULL,
-  cuisine_type VARCHAR (255) NOT NULL,
-  rating INTEGER,
-  start TIME WITH TIME ZONE NOT NULL,
-  "end" TIME WITH TIME ZONE NOT NULL,
-  date DATE NOT NULL,
-  price NUMERIC (6, 2) NOT NULL
-)`);
-
-pool.query(`CREATE TABLE IF NOT EXISTS user_events (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
-  event_id INTEGER REFERENCES events(id)
-)`);
-
+pool
+  .connect()
+  .then(client => client
+    .query(`CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY NOT NULL,
+      first_name VARCHAR (255) NOT NULL,
+      last_name VARCHAR (255) NOT NULL,
+      email VARCHAR (255) NOT NULL,
+      password VARCHAR (255) NOT NULL,
+      role VARCHAR (255) NOT NULL
+    )`)
+    .then(() => client.query(`CREATE TABLE IF NOT EXISTS events (
+      id SERIAL PRIMARY KEY NOT NULL,
+      title VARCHAR (255) NOT NULL,
+      description VARCHAR (500) NOT NULL,
+      max_attendees INTEGER NOT NULL,
+      host INTEGER REFERENCES users(id) NOT NULL,
+      location VARCHAR (255) NOT NULL,
+      cuisine_type VARCHAR (255) NOT NULL,
+      rating INTEGER,
+      start TIME WITH TIME ZONE NOT NULL,
+      "end" TIME WITH TIME ZONE NOT NULL,
+      date DATE NOT NULL,
+      price NUMERIC (6, 2) NOT NULL
+    )`))
+    .then(() => client.query(`CREATE TABLE IF NOT EXISTS user_events (
+      id SERIAL PRIMARY KEY NOT NULL,
+      user_id INTEGER REFERENCES users(id),
+      event_id INTEGER REFERENCES events(id)
+    )`))
+    .then(() => client.release())
+    .catch((e) => {
+      client.release();
+      console.log(e.stack);
+    }));
 
 /**
  * In order to prevent leaking clients, we will export the
