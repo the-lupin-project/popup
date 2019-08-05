1c9d6e6f054c45e1b03bebee1053547ccaf96e47
Neftali Dominguez <n.l.dominguez23@gmail.com>
2019-08-05 16:35:03 -0700

    added an events reducer

---
+++ client/reducers/eventsReducer.js
@@ -0,0 +1,20 @@
+import * as actionTypes from '../constants/actionTypes';
+
+const initialState = {
+  events: [],
+  title: '',
+  description: '',
+  maxAttendees: 0,
+  host: '',
+  location: '',
+  cuisineType: '',
+  rating: 0,
+  start: undefined,
+  end: undefined,
+  date: undefined,
+  price: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  
+}
