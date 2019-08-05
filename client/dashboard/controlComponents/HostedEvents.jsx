226cf74abe636c3da640fcddda504b976427f416
Jessica Vaughan <jessicavaughan@Jessicas-MacBook-Pro.local>
2019-08-05 09:52:42 -0700

    before connecting modal to the edit event btn

--- client/dashboard/controlComponents/HostedEvents.jsx
+++ client/dashboard/controlComponents/HostedEvents.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Card, Button, CardTitle, CardText } from 'reactstrap';
 
+import HostedEventModal from 'HostedEventModal.jsx';
+
 const HostedEvents = (props) => {
+  // function activateModal() {
+  //   // console.log('the link was clicked');
+  //   return (
+  //     <HostedEventModal />
+  //   );
+  // }
+
   return (
     <div>
       <Card body className="hostedEventCard">
         <div className="hostedEvents">
           <CardTitle>Event Title</CardTitle>
-          <Button>Edit Event</Button>
+          <Button onClick={activateModal}>Edit Event</Button>
         </div>
       </Card>
     </div>
