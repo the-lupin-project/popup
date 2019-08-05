0a22be4f1aafaa8007cd2ce0d17d60d79827c7ac
Jessica Vaughan <jessicavaughan@Jessicas-MacBook-Pro.local>
2019-08-05 10:47:28 -0700

    just before push

--- client/dashboard/controlComponents/HostedEvents.jsx
+++ client/dashboard/controlComponents/HostedEvents.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Card, Button, CardTitle, CardText } from 'reactstrap';
 
-import HostedEventModal from 'HostedEventModal.jsx';
+import HostedEventModal from './HostedEventModal.jsx';
 
 const HostedEvents = (props) => {
-  // function activateModal() {
-  //   // console.log('the link was clicked');
-  //   return (
-  //     <HostedEventModal />
-  //   );
-  // }
+  function activateModal() {
+    // console.log('the link was clicked');
+    return (
+      <HostedEventModal />
+    );
+  }
 
   return (
     <div>
       <Card body className="hostedEventCard">
         <div className="hostedEvents">
           <CardTitle>Event Title</CardTitle>
-          <Button onClick={activateModal}>Edit Event</Button>
+          <Button>Edit Event</Button>
         </div>
       </Card>
     </div>
