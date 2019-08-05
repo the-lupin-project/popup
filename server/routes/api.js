4f22942776e1b9cd3c1a69df64f1674aee52f42d
Simon-IM <smaharai@gmail.com>
2019-08-05 11:02:06 -0700

    added api.js and required in server.js

---
+++ server/routes/api.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const eventController = require('../controllers/controllers');
+
+const router = express.Router();
+
+// router.get('/', );
+
+
+module.exports = router;
