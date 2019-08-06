9fc8abf5c38cc44da112c6fb7ff4b30a91bc45cc
Simon-IM <smaharai@gmail.com>
2019-08-05 18:03:42 -0700

    modified controllers.js and api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,7 +4,9 @@ const eventController = require('../controllers/controllers');
 
 const router = express.Router();
 
-// router.get('/', );
+router.get('/', eventController.findAllEvents, (req, res) => {
+    res.status(200).json(res.locals.events);
+});
 
 
 module.exports = router;
