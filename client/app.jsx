c49d9fc7f84e813dcafd55f63f091185fb31ec24
Jacob <jacobjeffreybanks@gmail.com>
2019-08-05 18:35:15 -0700

    tried to implement react router

--- client/app.jsx
+++ client/app.jsx
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import {Provider} from "react-redux"
 import PageContainer from './LandingPage/Wrappers/PageContainer.jsx'
 import ModalContainer from './LandingPage/Wrappers/ModalsRootContainer.jsx'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import './application.scss';
 
 import Wrapper from './dashboard/Wrapper.jsx'
 
-export default function App ({ children }) {
+class App extends Component {
+  render() {
   return (
     <div id="main">
       <PageContainer />
       <Wrapper />
-      {children}
       <ModalContainer />
     </div>
   );
 }
+}
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -6,12 +6,15 @@ import styles from './LandingPage/scss/application.scss';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App.jsx';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 render(
   <Provider store = { store } >
+      <Router>
       <App / >
+      </Router>
   </Provider>,
   document.getElementById('root')
 );
